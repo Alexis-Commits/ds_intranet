c40ec6f2452db932aa743d8a7f2cc6e9222abf58
AlphaKappa32 <aggeloskar32@gmail.com>
2020-01-07 16:28:39 +0200

    HousingAlgorithm done

--- src/hua/algorithms/HousingAlgorithm.java
+++ src/hua/algorithms/HousingAlgorithm.java
@@ -5,6 +5,7 @@ import hua.objects.StudentsRequests;
 public class HousingAlgorithm {
     public static int calcScore(StudentsRequests request){
         int score= 0 ;
+        int maxScore = 1000;
 
         int income = request.getIncome();
         int studyingBrothers = request.getBrothers();
@@ -13,7 +14,29 @@ public class HousingAlgorithm {
         int alreadyHousingYears = request.getHousingYears();
 
         //TODO: Create algorithm
+        if(fromAnotherCity == 0 || semester > 8) {
+            return score;
+        }
+        else {
+            score += 50;
 
+            if (income == 0) {
+                score = maxScore;
+                return score;
+            }
+            else if(income <10000){
+                score += 100;
+            }
+            else if(income<15000){
+                score += 30;
+            }
+        }
+        for (int i=1; i<=studyingBrothers; i++ ){
+            score += 20;
+        }
+        for (int i = alreadyHousingYears; i>0; i--){
+            score -= 10;
+        }
         return score;
     }
 }
